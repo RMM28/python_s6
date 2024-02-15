2bedc40c6020cdebd448e9a35c6ecb80db6ba36b
RMM28 <116085532+RMM28@users.noreply.github.com>
2024-02-15 23:44:45 +0530

    Add files via upload

---
+++ Ex_1.py
@@ -0,0 +1,12 @@
+p=float(input("Enter the amount to find compund interest to: "))
+r=int(input("Enter the rate of interest as %: "))
+t=int(input("Enter the number of years: "))
+i_sum=0
+print("Year\tStarting Balance\tInterest\tEnding Balance")
+for i in range(t):
+    interest=float((p*r)/100)
+    print(i+1,"\t",p,"\t","%14.2f"%round(interest,2),"\t","%10.2f"%(p+interest))
+    p=p+interest
+    i_sum+=interest
+print("ending balance: $",round(p,2))
+print("Total interest earned: $",round(i_sum,2))
